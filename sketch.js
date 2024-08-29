de1eedf571564fddc423e3acb4c0de9c609d05e2
TheoO09 <theo@sonner.com.br>
2024-08-29 16:00:31 -0300

    Update 3.0

--- sketch.js
+++ sketch.js
@@ -1,32 +1,46 @@
+//Variáveis referentes a posição da bola
 let xBolinha = 300
 let yBolinha = 200
 
+//Variáveis referentes ao tamanho da bola
 let diametro = 25
 let raio = diametro / 2
 
+//Variáveis referentes as especificações da raquete
 let larguraRect = 17
 let alturaRect = 110
 
+//Variáveis referentes ao posicionamento incial da raquete do bot
 let xRectOp = 582
 let yRectOp = 135
 
+
+//Variáveis referentes ao posicionamento inicial da raquete do jogador
 let xMeuRect = 1
 let yMeuRect = 135
 
+//Variáveis referentes a velocidade da bolinha e angulação de início dela
 let velocidadeXbolinha = 9
 let velocidadeYbolinha = 3
 
+//Variável usada para parar o movimento da raquete do bot em caso de fuga do espaçamento jogável
 let pararRaqueteOp = false
 
+//Variáveis para determinar a quantidade de pontos do jogador e do bot
 let meusPontos = 0;
 let pontosOp = 0;
 
+//Variáveis dos sons usados no jogo
 let raquetada;
 let ponto;
 let trilha
 
-let dificuldadeBot = 100
+//Variáveis para determinar um atraso do tempo de reação do bot para aumentar ou diminuir a dificuldade
+let tempoReacaoBot = 0
+let delayBot = 5; //Determina a dificuldade
+let yObjetivoBot = 135;
 
+//Inclusão dos sons no jogo
 function preload(){
 
   raquetada = loadSound("raquetada.mp3");
@@ -34,11 +48,13 @@ function preload(){
   trilha = loadSound("trilha.mp3");
 }
 
+//Criar a bola
 function desenhaBola(){
   
   circle(xBolinha, yBolinha, diametro);
 }
 
+//Codar a movimentação da bola
 function movimentoBola(){
   
   xBolinha += velocidadeXbolinha;
@@ -47,6 +63,7 @@ function movimentoBola(){
 
 }
 
+//Cria a física de colisão com as paredes da bolinha
 function colisaoParede(){
   
   if(xBolinha + raio > width || xBolinha - raio < 0){
@@ -62,6 +79,7 @@ function colisaoParede(){
 
 }
 
+//Desenha as raquetes utilizando as variáveis de cima
 function raquetes(x, y){
   
   rect(x, y, larguraRect, alturaRect);
@@ -69,6 +87,7 @@ function raquetes(x, y){
 
 }
 
+//Estabelece os comandos de movimento da raquete do jogador
 function movimentoMeuRect(){
   
   if (keyIsDown(UP_ARROW)){
@@ -85,37 +104,55 @@ function movimentoMeuRect(){
   }
 }
 
+//Determina a física utilizada na colisão da raquete do jogador
 function colisaoRaquete() {
   if (xBolinha - raio < xMeuRect + larguraRect && 
       yBolinha - raio < yMeuRect + alturaRect && 
       yBolinha + raio > yMeuRect) {
       
     velocidadeXbolinha *= -1;
+    velocidadeXbolinha += 0.1;
     xBolinha = xMeuRect + larguraRect + raio;
     raquetada.play();
+    raquetada.setVolume(0.3);
   }
 }
 
+//Determina a física utilizada na colisão da raquete do bot
 function colisaoRaqueteOp() {
   if (xBolinha + raio > xRectOp && 
       yBolinha - raio < yRectOp + alturaRect && 
       yBolinha + raio > yRectOp) {
       
     velocidadeXbolinha *= -1;
+    velocidadeXbolinha += 0.1;
     xBolinha = xRectOp - raio;
     raquetada.play();
+    raquetada.setVolume(0.3);
   }
 }
 
-function movimentoRaqueteOp(){
+//Estabelece as condições de movimento do bot determinando o atraso de reação, assim a dificuldade
+function movimentoRaqueteOp() {
+  if (!pararRaqueteOp) return;
   
-  if(pararRaqueteOp){
-  velocidadeYop = yBolinha - yRectOp - larguraRect / 2 - dificuldadeBot;
-  yRectOp += velocidadeYop;
+  tempoReacaoBot += 2;
+  if (tempoReacaoBot > delayBot) {
+    yObjetivoBot = yBolinha - alturaRect / 2; 
+    tempoReacaoBot = 0; 
   }
-  
+
+  let velocidadeYop = yObjetivoBot - yRectOp;
+  yRectOp += velocidadeYop * 0.1;
+
+  if (yRectOp + alturaRect > height) {
+    yRectOp = height - alturaRect;
+  } else if (yRectOp < -30) {
+    yRectOp = 0;
+  }
 }
 
+//Questão estética do placar do jogo sofrerá alterações em breve
 function placarJogo(){
   
   stroke(255);
@@ -130,24 +167,29 @@ function placarJogo(){
   text(pontosOp, 420, 28);
 }
 
+//Função para aumentar a variável dos pontos ao tocar na parede e tocar uma trilha sonora 
 function marcaPonto(){
   
   if(xBolinha - raio < 1){
     pontosOp ++
     ponto.play();
+    ponto.setVolume(0.2);
   }
   if(xBolinha + raio > 600){
     meusPontos ++
     ponto.play();
+    ponto.setVolume(0.2);
   }
 }
 
-
+//Função criada pela API do p5.js editor estabelece o ambiente para o jogo e inicia a trilha sonora
 function setup() {
   createCanvas(600, 400);
   trilha.loop();
+  trilha.setVolume(0.1);
 }
 
+//Função também criada pela API do p5.js editor que executa as funções dentro infinitamente
 function draw() {
   background(0);
   
@@ -171,7 +213,8 @@ function draw() {
   placarJogo();
   marcaPonto();
   
-  if(yBolinha > 390 || yBolinha < 110){
+  //Verifica a posição da bolinha para que a raquete do oponente não saia da área jogável
+  if(yBolinha > 390 || yBolinha < 50){
     
     pararRaqueteOp = false;
   } else {
